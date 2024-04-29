e57801757414580029362e13028ff014a380837a
Gerard Altadill <gerard.altadill@estudiants.urv.cat>
2024-04-29 14:12:41 +0200

    Commit 4t dia gerard

--- Client.py
+++ Client.py
@@ -5,96 +5,114 @@ import threading
 import grpc
 
 import chatservice_pb2
-import chatservice_pb2_grpc # Importa tu archivo .proto generado
+import chatservice_pb2_grpc  # Importa tu archivo .proto generado
 
 nom = ""
-channel = grpc.insecure_channel('localhost:50051')
-stub = chatservice_pb2_grpc.ChatServiceStub(channel)
-
-def getStub():
-    return stub
-
-def demanar_dades():
-    global nom
-    print("Benvingut al servei de xats")
-    nom = input("Introdueix el teu nom: ")
-    stub.Connect(chatservice_pb2.ConnectRequest(username=nom))
-
-def mostrar_menu():
-    print("Benvingut al servei de xats, " + nom)
-    print("1. Connecta el xat")
-    print("2. Subscriu-te al xat de grup")
-    print("3. Descobreix xats")
-    print("4. Accedeix al canal d'insults")
-    print("5. Mostrar missatges rebuts")
-    print("6. Sortir")
 
 
+class Client:
 
-def opcio1():
-    print("1")
-    global nom
-    message = input("Introdueix el missatge: ")
-    receiver = input("A qui li vols enviar el missatge?  ")
-    nom2 = nom
-    response = stub.SendMessage(chatservice_pb2.MessageRequest(sender=nom2, receiver=receiver, message=message))
-    print(response.message)
+    def __init__(self, nom2):
+        self.nom = nom2
+        self.channel = grpc.insecure_channel('localhost:50051')
+        self.stub = chatservice_pb2_grpc.ChatServiceStub(self.channel)
 
-def opcio2():
-    print("2")
+    def receive_messages(self, request):
+        try:
+            for response in self.stub.ReceiveMessage(
+                    chatservice_pb2.MessageRequest(request)):
+                print(f"Mensaje recibido: De {response.sender}: {response.message}")
+        except grpc.RpcError as e:
+            print(f"Error al recibir mensajes: {e}")
 
+    def getStub(self):
+        return self.stub
 
-def opcio3():
-    print("3")
+    def demanar_dades(self):
+        global nom
+        print("Benvingut al servei de xats")
+        nom = input("Introdueix el teu nom: ")
+        self.stub.Connect(chatservice_pb2.ConnectRequest(username=nom))
 
+    def mostrar_menu(self):
+        print("Benvingut al servei de xats, " + nom)
+        print("1. Connecta el xat")
+        print("2. Subscriu-te al xat de grup")
+        print("3. Descobreix xats")
+        print("4. Accedeix al canal d'insults")
+        print("5. Mostrar missatges rebuts")
+        print("6. Sortir")
 
-def opcio4():
-    print("4")
+    def opcio1(self):
+        print("1")
+        global nom
+        message = input("Introdueix el missatge: ")
+        receiver = input("A qui li vols enviar el missatge?  ")
+        nom2 = nom
+        response = self.stub.SendMessage(
+            chatservice_pb2.MessageRequest(sender=nom2, receiver=receiver, message=message))
+        #print(response.message)
 
-def opcio5():
-    print("5")
-    receptor = input("Introdueix de qui vols llegir els missatges: ")
-    response = stub.ReceiveMessage(chatservice_pb2.MessageRequest(sender=nom, receiver=receptor, message="ola"))
-    print(response.message)
+    def opcio2(self):
+        print("2")
 
-def main():
-    demanar_dades()
-    while True:
-        mostrar_menu()
-        opcio = input("Elegeix una opció: ")
+    def opcio3(self):
+        print("3")
 
-        if opcio == "1":
-            print("Has elegit Connecta el xat")
-            opcio1()
-        elif opcio == "2":
-            print("Has elegit Subscriu-te al xat de grup")
-            opcio2()
-        elif opcio == "3":
-            print("Has elegit Descobreix xats")
-            opcio3()
-        elif opcio == "4":
-            print("Has elegit Accedeix al canal d'insults")
-            opcio4()
-        elif opcio == "5":
-            print("Has elegit Mostrar missatges rebuts")
-            opcio5()
-        elif opcio == "6":
-            print("Sortint...")
-            break
-        else:
-            print("Opció no valida. Si us plau, elegeix una opció")
+    def opcio4(self):
+        print("4")
+
+    def opcio5(self, client):
+        print("5")
+        receive_thread = threading.Thread(target=client.receive_messages, daemon=True)
+        receive_thread.start()
+
+        while True:
+            receiver = input("Introduce el destinatario del mensaje: ")
+            message = input("Introduce el mensaje: ")
+            client.send_message(receiver, message)
+
+    def start(self):
+        global nom
+        self.demanar_dades()
+
+        while True:
+            self.mostrar_menu()
+            opcio = input("Elegeix una opció: ")
+
+            if opcio == "1":
+                print("Has elegit Connecta el xat")
+                self.opcio1()
+            elif opcio == "2":
+                print("Has elegit Subscriu-te al xat de grup")
+                self.opcio2()
+            elif opcio == "3":
+                print("Has elegit Descobreix xats")
+                self.opcio3()
+            elif opcio == "4":
+                print("Has elegit Accedeix al canal d'insults")
+                self.opcio4()
+            elif opcio == "5":
+                print("Has elegit Mostrar missatges rebuts")
+                self.opcio5(client)
+            elif opcio == "6":
+                print("Sortint...")
+                break
+            else:
+                print("Opció no valida. Si us plau, elegeix una opció")
 
 
 if __name__ == "__main__":
-    main()
+    client = Client(nom)
+    client.start()
     # Crea los procesos para cada cliente
 #    proceso_cliente1 = threading.Thread(target=main())
 #    proceso_cliente2 = threading.Thread(target=main())
 
-    # Inicia los procesos
+# Inicia los procesos
 #    proceso_cliente1.start()
 #    proceso_cliente2.start()
 
-    # Espera a que los procesos terminen
+# Espera a que los procesos terminen
 #    proceso_cliente1.join()
 #    proceso_cliente2.join()
--- Server.py
+++ Server.py
@@ -12,15 +12,19 @@ class ChatService(chatservice_pb2_grpc.ChatServiceServicer):
         sender = context.peer()
         receiver = request.receiver
         message = request.message
-        print(f"Missatge enviat de {sender} a {receiver}: {message}")
+        username = request.sender
+        print(f"Missatge enviat de {username} a {receiver}: {message}")
 
         if receiver in self.clients:
-            for client in self.clients[receiver]:
-                channel = grpc.insecure_channel(client.peer())
-                client_stub = chatservice_pb2_grpc.ChatServiceStub(channel)
-                client_stub.ReceiveMessage(chatservice_pb2.MessageRequest(sender=sender, receiver=receiver, message=message))
-                #client.SendMessage(chatservice_pb2.MessageRequest(sender=sender, receiver=receiver, message=message))
+            channel = grpc.insecure_channel(sender)
+            client_stub = chatservice_pb2_grpc.ChatServiceStub(channel)
+            client_stub.SendMessage(chatservice_pb2.MessageRequest(sender=username, receiver=receiver, message=message))
             return chatservice_pb2.MessageResponse(message="Missatge enviat al destinatari")
+            #for client in self.clients[receiver]:
+                #channel = grpc.insecure_channel(sender)
+                #client_stub = chatservice_pb2_grpc.ChatServiceStub(channel)
+                #client_stub.SendMessage(chatservice_pb2.MessageRequest(sender=username, receiver=receiver, message=message))
+            #return chatservice_pb2.MessageResponse(message="Missatge enviat al destinatari")
         else:
             return chatservice_pb2.MessageResponse(message=f"No s'ha pogut entregar el missatge {receiver}")
 
@@ -40,7 +44,7 @@ class ChatService(chatservice_pb2_grpc.ChatServiceServicer):
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     chatservice_pb2_grpc.add_ChatServiceServicer_to_server(ChatService(), server)
-    server.add_insecure_port('[::]:50051')
+    server.add_insecure_port('localhost:50051')
     server.start()
     print("Servidor gRPC en execució al port 50051")
     server.wait_for_termination()
